ecbce7d43d78757f158fc86fade303a9dfa9c795
Damian Kanski <damiankanski4@gmail.com>
2023-10-17 22:00:57 +0200

    feat/bRAGv2-005-ednpoints: create first endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from app import version
+from app.core.api import router as core_router
 from app.core.logs import logger
 
 app = FastAPI(version=version)
+app.include_router(core_router)
 logger.info("App is ready!")
