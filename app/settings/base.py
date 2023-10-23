04821b1e1524e8a0e3039b083db84867975f5c7f
Damian Kanski <damiankanski4@gmail.com>
2023-10-23 14:54:49 +0200

    feat/bRAGv2-010-qdrant: add qdant values to base.py

--- app/settings/base.py
+++ app/settings/base.py
@@ -5,11 +5,16 @@ from app.core.constants import Environments
 
 
 class Settings(BaseSettings):
-    APP_NAME: str = "bRAG"
+    APP_NAME: str = "bRAGv2"
     ENVIRONMENT: str = Field(env="ENVIRONMENT", default=Environments.LOCAL.value)
 
+    DATABASE_URL: str = Field(env="DATABASE_URL")
     OPENAI_API_KEY: str = Field(env="OPENAI_API_KEY")
     GENERATION_TIMEOUT_SEC: int = Field(env="GENERATION_TIMEOUT_SEC", default=120)
+
+    QDRANT_HOST: str = Field(env="QDRANT_HOST", default="localhost")
+    QDRANT_API_KEY: str = Field(env="OPENAI_API_KEY", default="None")
+    QDRANT_COLLECTION_NAME: str = Field(env="QDRANT_COLLECTION_NAME", default="demo")
     @property
     def is_local(self):
         return self.ENVIRONMENT == Environments.LOCAL.value
